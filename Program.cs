4695c23e48711176953ad6bf459e83f8aafcf603
GiangVu-2002 <gvu2883@gmail.com>
2024-10-07 16:37:29 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bài_tập_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2, y = 4;
+            Console.WriteLine(x+y);
+            Console.WriteLine(x-y);
+            Console.WriteLine(x*y);
+            Console.ReadKey();
+        }
+    }
+}
